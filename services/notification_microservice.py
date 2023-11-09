30c4c0d2343c118e7f98b2c57f633887eef007a9
unknown <ffarzam_1992@yahoo.com>
2023-11-09 17:26:23 +0330

    refactor: update notification microservise

--- services/notification_microservice.py
+++ services/notification_microservice.py
@@ -1,13 +1,16 @@
-import json
-
 import httpx
+
+from config.config import get_settings
+
+settings = get_settings()
 
 
 class Notification:
-    code_sender_url = "http://127.0.0.1:8002/notification/v1/code_sender"
+    code_sender_url = settings.NOTIFICATION_CODE_SENDER
 
-    async def code_call(self, payload: dict):
-        async with httpx.AsyncClient() as client:
+    async def code_call(self, payload: dict, request):
+        headers = {"unique_id": request.state.unique_id}
+        async with httpx.AsyncClient(headers=headers) as client:
             response = await client.post(self.code_sender_url, json=payload)
         # response.raise_for_status()
         return response
